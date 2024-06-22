6fd6627c21a7a72355cc3749bd7d45b960eae2b4
39takagikouta <kouta.takagi0728@gmail.com>
2024-06-23 00:26:35 +0900

    課題完了

---
+++ fizz_buzz.rb
@@ -0,0 +1,13 @@
+def fizz_buzz(number)
+  if number % 15 == 0
+  "Fizz Buzz"
+  elsif number % 3 == 0
+    "Fizz"
+  elsif number % 5 == 0
+    "Buzz"
+  else
+    number
+  end
+end
+
+puts fizz_buzz(1)
